18055f5eeddc50b323489ee18b452d88361d50f4
Gaspar Pelayo <gasparpelayoimp@gmail.com>
2023-06-29 12:24:47 -0300

    Add files via upload

---
+++ 1/01/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* Exercise 1-1. Run the "hello, world" program on your system. Experiment
+with leaving out parts of the program, to see what error messages you get. */
+
+int main() {
+    printf("Esto es \"La COLIMBA!\"\n");
+    return 0;
+}
