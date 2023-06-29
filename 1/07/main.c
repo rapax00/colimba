f347366e43760b94000c6f1e3406d418d8f1ffbd
Gaspar Pelayo <gasparpelayoimp@gmail.com>
2023-06-29 12:30:08 -0300

    Add files via upload

---
+++ 1/07/main.c
@@ -0,0 +1,11 @@
+	#include <stdio.h>
+
+/* Exercise 1-7. Write a program to print the value of EOF */
+
+int main(){
+    int c = EOF;
+
+    printf("El valor de EOF es: %d\n", c);
+    
+    return 0;
+}
