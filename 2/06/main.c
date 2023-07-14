473fee0519a1ceb5487799573c8259fd1152a753
Gaspar Pelayo <gasparpelayoimp@gmail.com>
2023-07-14 17:55:11 -0300

    Add files via upload

--- 2/06/main.c
+++ 2/06/main.c
@@ -1,22 +1,24 @@
-/* Exercise 2-7. Write a function invert ( x , p , n ) that returns x with the n bits
-that begin at position p inverted (i.e., 1 changed into 0 and vice versa), leaving
-the others unchanged.*/
+/* Exercise 2-6. Write a function setbits(x,p,n,y) that returns x with the n
+bits that begin at position p set to the rightmost n bits of y, leaving the other
+bits unchanged. */
 
-#include <stdio.h>
-
-int invert(int x, int p, int n) {
+int setbits(int x, int p, int n, int y) {
     int aux;
 
-    aux = (1 << p) -1;      /* obtain distance p of bit to change */
-    aux = aux >> (p - n);   /* set only bits to change */
-    aux = aux << (p - n);   /* obtain distance and really bits to invert of x */
+    aux = (1 << n) - 1;     /* define number of bits that I get of y */
 
-    return x ^ aux;
+    int yaux = y & aux;     /* obtain finaly bits to put in x */
+
+    yaux = yaux << (p-n);   /* positions in the p position the bits to put in x */
+
+    return x | yaux;        /* put the bits in x */
 }
 
-/* Test */
+/* Test*/
+/*
 int main() {
-    printf("%d", invert(32, 5, 3));
+    printf("%d", setbits(42, 3, 2, 11));
 
     return 0;
 }
+*/
