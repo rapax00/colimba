68d14ca5290b8c1b5e339b085a9bbaf996a749bb
Gaspar Pelayo <gasparpelayoimp@gmail.com>
2023-09-02 20:56:26 -0300

    Add files via upload

---
+++ 5/04/functions.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTIONS_INCLUDED
+#define FUNCTIONS_INCLUDED
+
+char *strncpy(char [], char [], int);
+char *strncat(char [], char [], int);
+int strncmp(char [], char [], int);
+
+#endif // FUNCTIONS_INCLUDED
--- 5/04/main.c
+++ 5/04/main.c
@@ -1,31 +1,36 @@
-/* Exercise 5-4. Write the function strend(s,t), which returns 1 if the string
-t occurs at the end of the string s, and zero otherwise. */
+/* Exercise 5-5. Write versions of the library functions strncpy, strncat, and
+strncmp, which operate on at most the first n characters of their argument
+strings. For example, strncpy(s,t,n) copies at most n characters of t to s.
+Full descriptions are in Appendix B. */
 
 #include <stdio.h>
-#include <string.h>
 
-int strend(char *, char *);
+#define MAX 50
+#define MAGIC_NUMBER 4
 
 int main(void) {
-    char a[] = "hola carola";
-    char b[] = "carola";
+    char a[] = "casiron";
+    char b[] = "lolo";
+    char *c;
 
-    printf("Strings:\na: %s\nb: %s\n", a, b);
-    printf("---\n\"a\" occurs at the end of \"a\"? (1 for YES, 0 for NO)\n%d", strend(a, b));
+    /* strncpy */
+    printf("STRNCPY\nBefore\na: %s\nb: %s\n", a, b);
+    c = strncpy(a, b, MAGIC_NUMBER);
+    printf("After\na: %s\nb: %s\nc: %s\n---\n", a, b, c);
+
+    /* strncat */
+    char d[MAX] = "caseron";    /* it's necessary set size of array because should be add more chars in at */
+    char e[] = "eza";
+
+    printf("STRNCAT\nBefore\nd: %s\ne: %s\n", d, e);
+    c = strncat(d, e, MAGIC_NUMBER);
+    printf("After\nd: %s\ne: %s\nc: %s\n---\n", d, e, c);
+
+    /* strncmp */
+    char f[] = "caseron";
+    char g[] = "casa";
+
+    printf("STRNCMP\nd: %s\ne: %s\nresult: %d", f, g, strncmp(f, g, MAGIC_NUMBER));
 
     return 0;
-}
-
-int strend(char *s, char *t) {
-    int i, j;
-
-    for (i = strlen(s) - 1, j = strlen(t) - 1; 0 <= j; i--, j--) {
-        if (*(s + i) == *(t + j)){
-            ;
-        } else {
-            return 0;
-        }
-    }
-
-    return 1;
 }
