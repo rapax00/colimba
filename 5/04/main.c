600c3834967df84c2e75cf916f1c874ab9f14ecd
Gaspar Pelayo <gasparpelayoimp@gmail.com>
2023-09-02 20:57:37 -0300

    Add files via upload

--- 5/04/main.c
+++ 5/04/main.c
@@ -1,36 +1,33 @@
-/* Exercise 5-5. Write versions of the library functions strncpy, strncat, and
-strncmp, which operate on at most the first n characters of their argument
-strings. For example, strncpy(s,t,n) copies at most n characters of t to s.
-Full descriptions are in Appendix B. */
+/* Exercise 5-4. Write the function strend(s,t), which returns 1 if the string
+t occurs at the end of the string s, and zero otherwise. */
 
 #include <stdio.h>
+#include <string.h>
 
-#define MAX 50
-#define MAGIC_NUMBER 4
+int strend(char *, char *);
 
 int main(void) {
-    char a[] = "casiron";
-    char b[] = "lolo";
-    char *c;
+    char b[] = "hola carola";
+    char a[] = "carola";
 
-    /* strncpy */
-    printf("STRNCPY\nBefore\na: %s\nb: %s\n", a, b);
-    c = strncpy(a, b, MAGIC_NUMBER);
-    printf("After\na: %s\nb: %s\nc: %s\n---\n", a, b, c);
-
-    /* strncat */
-    char d[MAX] = "caseron";    /* it's necessary set size of array because should be add more chars in at */
-    char e[] = "eza";
-
-    printf("STRNCAT\nBefore\nd: %s\ne: %s\n", d, e);
-    c = strncat(d, e, MAGIC_NUMBER);
-    printf("After\nd: %s\ne: %s\nc: %s\n---\n", d, e, c);
-
-    /* strncmp */
-    char f[] = "caseron";
-    char g[] = "casa";
-
-    printf("STRNCMP\nd: %s\ne: %s\nresult: %d", f, g, strncmp(f, g, MAGIC_NUMBER));
+    printf("Strings:\na: %s\nb: %s\n", a, b);
+    printf("---\n\"b\" occurs at the end of \"a\"? (1 for YES, 0 for NO)\n%d", strend(a, b));
 
     return 0;
+}
+
+int strend(char *s, char *t) {
+    int i, j;
+
+    for(i = strlen(s) - 1, j = strlen(t) - 1; 0 <= j; i--, j--) {
+        if(i < 0){
+            return 0;
+        } else if(*(s + i) == *(t + j)) {
+            ;
+        } else {
+            return 0;
+        }
+    }
+
+    return 1;
 }
