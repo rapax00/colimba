6016f8b7971f20316be1fcd871ffe92d6d1f1e8a
Gaspar Pelayo <gasparpelayo44@gmail.com>
2023-09-02 20:57:22 -0300

    Delete 5/04/functions.h

--- 5/04/functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCTIONS_INCLUDED
-#define FUNCTIONS_INCLUDED
-
-char *strncpy(char [], char [], int);
-char *strncat(char [], char [], int);
-int strncmp(char [], char [], int);
-
-#endif // FUNCTIONS_INCLUDED
